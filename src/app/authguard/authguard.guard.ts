02560a9bccb323750e8dc48ef9b3345f68c19067
ketan_vaidya <ketan.vaidya@platforminstruments.com>
2020-12-31 08:45:25 +0530

    autguard change

--- src/app/authguard/authguard.guard.ts
+++ src/app/authguard/authguard.guard.ts
@@ -9,11 +9,17 @@ import { LoginService } from '../services/login.service';
 export class AuthguardGuard implements CanActivate {
   constructor(private loginService:LoginService,private router:Router){}
 
-  canActivate(
+   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+   
+    if(this.loginService.isLoggedIn()){
+      return true;
+    }
+
     this.router.navigate(['/']);
-    return this.loginService.isLoggedIn();
+    return false;
   }
+  
   
 }
